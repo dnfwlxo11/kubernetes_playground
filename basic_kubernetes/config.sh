28a6cb131d9e9146283f9bc34ebf956afa3268d8
dnfwlxo11 <dnfwlxo11@naver.com>
2022-07-07 17:58:31 +0900

    add: basic kube example

--- basic_kubernetes/config.sh
+++ basic_kubernetes/config.sh
@@ -32,8 +32,8 @@ EOF
 modprobe br_netfilter
 
 # local small dns & vagrant cannot parse and delivery shell code.
-echo "192.168.1.10 m-k8s" >> /etc/hosts
-for (( i=1; i<=$1; i++  )); do echo "192.168.1.10$i w$i-k8s" >> /etc/hosts; done
+echo "192.168.1.10 daein-k8s" >> /etc/hosts
+for (( i=1; i<=$1; i++  )); do echo "192.168.1.10$i daein-k8s-$i" >> /etc/hosts; done
 
 # config DNS  
 cat <<EOF > /etc/resolv.conf
---
+++ single_machine/init_package.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# init_package.sh
+
+yum install epel-release -y
+yum install vim-enhanced -y

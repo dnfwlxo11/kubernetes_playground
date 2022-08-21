f35f15bc91116abaab387a5fbf2ec4cbf8a97980
dnfwlxo11 <dnfwlxo11@naver.com>
2022-08-22 01:06:52 +0900

    add: every-can-ai project setting

---
+++ every-can-ai/kompose/volumes/flask/app/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo ${BRANCH_NAME}
+
+echo "git setting start"
+
+cd /app/train_server
+
+git checkout ${BRANCH_NAME}
+
+git reset --hard HEAD
+git pull origin ${BRANCH_NAME}
+
+echo "git ${BRANCH_NAME} pull completed"
+
+celery multi start -A job worker --workdir="./asyncFlask" --loglevel=debug --logfile="./%n%I.log" --pidfile="./%n.pid" --autoscale=2,1 --max-tasks-per-child=1
+
+python model_download.py
+python app.py
